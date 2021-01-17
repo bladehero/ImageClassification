d9d5c25127d39e2253bbab4f4a385cc44a4ce71a
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2021-01-17 03:13:26 +0200

    Upgraded .NET 5 version + Added Preparation_Train_Scenario project

---
+++ ImageClassification.Preparation_Train_Scenario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageClassification.Preparation_Train_Scenario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
