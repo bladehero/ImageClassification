441cbfcf87bab683eba755f0d4333a2029b04f65
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2021-01-26 15:15:42 +0200

    BottomBar implementation

--- ImageClassification.Vue/app/src/App.vue
+++ ImageClassification.Vue/app/src/App.vue
@@ -2,17 +2,20 @@
   <v-app>
     <TopBar />
     <v-main> </v-main>
+    <BottomBar />
   </v-app>
 </template>
 
 <script>
 import TopBar from '@/components/layouts/TopBar'
+import BottomBar from '@/components/layouts/BottomBar'
 
 export default {
   name: 'App',
 
   components: {
-    TopBar
+    TopBar,
+    BottomBar
   },
 
   data: () => ({})
---
+++ ImageClassification.Vue/app/src/components/layouts/BottomBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-bottom-navigation v-model="value" dark grow>
+    <v-btn value="Storage">
+      <span>Storage</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn value="Classification">
+      <span>Classification</span>
+      <v-icon>mdi-cube-scan</v-icon>
+    </v-btn>
+
+    <v-btn value="Classifiers">
+      <span>Classifiers</span>
+      <v-icon>mdi-lightbulb-on-outline</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data: () => ({ value: 'Storage' })
+}
+</script>
+
+<style lang="stylus"></style>
