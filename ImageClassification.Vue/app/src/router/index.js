466a106b74bb79e796a63d330f92fa1db4999691
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2021-01-26 15:00:39 +0200

    Skeleton structure of Home page

--- ImageClassification.Vue/app/src/router/index.js
+++ ImageClassification.Vue/app/src/router/index.js
@@ -9,6 +9,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/',
+    name: 'Classification',
+    component: () => import('@/views/Classification')
+  },
+  {
+    path: '/',
+    name: 'Classifiers',
+    component: () => import('@/views/Classifiers')
   }
 ]
 
--- ImageClassification.Vue/app/src/views/Home.vue
+++ ImageClassification.Vue/app/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2>Home</h2>
   </div>
 </template>
 
