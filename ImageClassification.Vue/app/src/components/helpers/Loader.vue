d52cd9037ce4879ee9af097edaaff669ae87426d
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2021-01-28 02:01:18 +0200

    Fix Loader

--- ImageClassification.Vue/app/src/components/helpers/Loader.vue
+++ ImageClassification.Vue/app/src/components/helpers/Loader.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="vld-parent">
     <Loading
       :active.sync="getIsLoading"
       :can-cancel="false"
-      :is-full-page="true"
+      :is-full-page="false"
+      loader="bars"
+      :lock-scroll="true"
     />
-  </div>
 </template>
 
 <script>
@@ -14,11 +14,6 @@ import { mapGetters } from 'vuex'
 import 'vue-loading-overlay/dist/vue-loading.css'
 
 export default {
-  data () {
-    return {
-      fullPage: false
-    }
-  },
   components: {
     Loading
   },
