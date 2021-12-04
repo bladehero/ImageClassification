691d5f4a269288ac1326546d7096501ffaca70ca
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2021-12-04 15:45:00 +0200

    fix

--- ImageClassification.Train/Program.cs
+++ ImageClassification.Train/Program.cs
@@ -21,15 +21,17 @@ namespace ImageClassification.Train
             Console.WriteLine("Scenario `{0}` has been started", typeof(Program).Assembly.GetName().Name);
 
             #region Paths
+
             var currentDirectory = Directory.GetCurrentDirectory();
             var projectDirectory = Path.GetFullPath(Path.Combine(currentDirectory, "..", "..", ".."));
             var source = Path.Combine(projectDirectory, "assets", "inputs", "images", "data-set.zip");
             var destination = Path.Combine(projectDirectory, "assets", "outputs", "classifier.zip");
+
             #endregion
 
             DownloadDataSet(source);
 
-            IDefaultTrainWrapper trainer = new DefaultTrainWrapper(source)
+            var trainer = new DefaultTrainWrapper(source)
             {
                 MeasureTime = true
             };
@@ -39,28 +41,32 @@ namespace ImageClassification.Train
             trainer.MulticlassMetricsUpdated += ConsoleMulticlassMetricsUpdated;
             trainer.ProgressChanged += ConsoleProgressChanged;
 
-            Directory.CreateDirectory(Path.GetDirectoryName(destination));
+            Directory.CreateDirectory(Path.GetDirectoryName(destination)!);
             if (File.Exists(destination))
             {
                 File.Delete(destination);
             }
+
             var classifications = await trainer.TrainAsync(destination);
 
             #region Results
+
             Console.WriteLine();
             Console.WriteLine("Scenario `{0}` has been finished {1}",
-                              typeof(Program).Assembly.GetName().Name,
-                              (classifications.Count() > 0 ? "successfully" : "with failure, check the logs."));
+                typeof(Program).Assembly.GetName().Name,
+                (classifications.Count() > 0 ? "successfully" : "with failure, check the logs."));
             Console.WriteLine();
+
             #endregion
         }
 
         #region Handlers
+
         private static void ConsoleImageMetricsUpdated(ImageClassificationMetrics metrics)
         {
             Console.WriteLine(new string('*', 30));
 
-            if (metrics.Train is TrainMetrics trainMetrics)
+            if (metrics.Train is { } trainMetrics)
             {
                 Console.WriteLine("Accuracy: {0}", trainMetrics.Accuracy);
                 Console.WriteLine("- Accuracy of the batch on this Epoch. Higher the better.");
@@ -78,7 +84,7 @@ namespace ImageClassification.Train
                 Console.WriteLine("- Learning Rate used for this Epoch. Changes for learning rate scheduling.");
             }
 
-            if (metrics.Bottleneck is BottleneckMetrics bottleneckMetrics)
+            if (metrics.Bottleneck is { } bottleneckMetrics)
             {
                 Console.WriteLine("Type of metrics: {0}", bottleneckMetrics.DatasetUsed);
 
@@ -88,23 +94,30 @@ namespace ImageClassification.Train
             Console.WriteLine(new string('*', 30));
             Console.WriteLine();
         }
+
         private static void ConsoleLog(object sender, LoggingEventArgs e)
         {
             Console.WriteLine("~~~ {0}", e.Message);
         }
+
         private static void ConsoleMulticlassMetricsUpdated(MulticlassClassificationMetrics metrics)
         {
             Console.WriteLine(new string('=', 30));
 
             Console.WriteLine("Log-loss: {0}", metrics.LogLoss);
-            Console.WriteLine("Log-loss measures the performance of a classifier with respect to how much the predicted probabilities diverge from the true class label. Lower log-loss indicates a better model. A perfect model, which predicts a probability of 1 for the true class, will have a log-loss of 0.");
+            Console.WriteLine(
+                "Log-loss measures the performance of a classifier with respect to how much the predicted probabilities diverge from the true class label. Lower log-loss indicates a better model. A perfect model, which predicts a probability of 1 for the true class, will have a log-loss of 0.");
             Console.WriteLine("Log-loss Reduction: {0}", metrics.LogLossReduction);
-            Console.WriteLine("It gives a measure of how much a model improves on a model that gives random predictions. Log-loss reduction closer to 1 indicates a better model.");
+            Console.WriteLine(
+                "It gives a measure of how much a model improves on a model that gives random predictions. Log-loss reduction closer to 1 indicates a better model.");
             Console.WriteLine("Macro Accuracy: {0}", metrics.MacroAccuracy);
-            Console.WriteLine("The accuracy for each class is computed and the macro-accuracy is the average of these accuracies. The macro-average metric gives the same weight to each class, no matter how many instances from that class the dataset contains.");
+            Console.WriteLine(
+                "The accuracy for each class is computed and the macro-accuracy is the average of these accuracies. The macro-average metric gives the same weight to each class, no matter how many instances from that class the dataset contains.");
             Console.WriteLine("Micro Accuracy: {0}", metrics.MicroAccuracy);
-            Console.WriteLine("The micro-average is the fraction of instances predicted correctly across all classes. Micro-average can be a more useful metric than macro-average if class imbalance is suspected.");
-            Console.WriteLine("This is the relative number of examples where the true label one of the top K predicted labels by the predictor.");
+            Console.WriteLine(
+                "The micro-average is the fraction of instances predicted correctly across all classes. Micro-average can be a more useful metric than macro-average if class imbalance is suspected.");
+            Console.WriteLine(
+                "This is the relative number of examples where the true label one of the top K predicted labels by the predictor.");
             Console.WriteLine("Top K Prediction Count: {0}", metrics.TopKPredictionCount);
             Console.WriteLine("Top K Accuracy: {0}", metrics.TopKAccuracy);
             Console.WriteLine("If positive, this indicates the K in Top K Accuracy and Top K Accuracy for all K.");
@@ -112,6 +125,7 @@ namespace ImageClassification.Train
             {
                 Console.WriteLine("Top K Accuracy for all K: ({0})", string.Join(", ", metrics.TopKAccuracyForAllK));
             }
+
             if (metrics.PerClassLogLoss?.Count > 0)
             {
                 Console.WriteLine("Per Class Log-loss: ({0})", string.Join(", ", metrics.PerClassLogLoss));
@@ -123,12 +137,13 @@ namespace ImageClassification.Train
 
             Console.WriteLine(new string('=', 30));
         }
+
         private static void ConsoleProgressChanged(TrainProgress progress)
         {
             Console.WriteLine();
             Console.WriteLine(new string('-', 30));
 
-            if (progress.Current is StepName stepName)
+            if (progress.Current is { } stepName)
             {
                 Console.WriteLine("[{0}]", DateTime.Now);
                 Console.WriteLine("Current step: {0}", stepName);
@@ -149,7 +164,7 @@ namespace ImageClassification.Train
 
             Console.WriteLine("Message: {0}", progress.Message);
 
-            if (progress.Elapsed is TimeSpan elapsed)
+            if (progress.Elapsed is { } elapsed)
             {
                 Console.WriteLine("Step took: {0}", elapsed);
             }
@@ -157,17 +172,20 @@ namespace ImageClassification.Train
             Console.WriteLine(new string('-', 30));
             Console.WriteLine();
         }
+
         #endregion
 
         #region Helpers
+
         private static void DownloadDataSet(string fileName)
         {
-            const string zip = @"https://github.com/bladehero/ImageClassification.DataSets/blob/master/data-set.zip?raw=true";
+            const string zip =
+                @"https://github.com/bladehero/ImageClassification.DataSets/blob/master/data-set.zip?raw=true";
             const float percentPerSection = 0.02f;
             const int total = 1;
             var progress = new Progress<float>();
             Console.WriteLine($"Downloading archive:");
-            Console.Write(new string('-', (int)(total / percentPerSection)));
+            Console.Write(new string('-', (int) (total / percentPerSection)));
             Console.SetCursorPosition(0, Console.CursorTop);
 
             var _lock = new object();
@@ -186,6 +204,7 @@ namespace ImageClassification.Train
             Console.WriteLine();
             Console.WriteLine($"Downloaded");
         }
+
         #endregion
     }
 }
